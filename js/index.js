e27ab95320755f57495213065946bf4009713d12
Phan Thanh Nhân <111231058+nhantubtr@users.noreply.github.com>
2023-11-04 21:56:18 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
+// Sửa lỗi khi nhập mật khẩu tự động zoom lại làm lệch web
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+  }
+});
 
