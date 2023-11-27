afd555413f042ff4108babb62562812212fafd64
Phan Thanh Nhân <111231058+nhantubtr@users.noreply.github.com>
2023-11-27 20:03:21 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,2 @@
-// Sửa lỗi khi nhập mật khẩu tự động zoom lại làm lệch web
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-  }
-});
-
+const botToken = "1234567890:ABCDEFGHIJKLMNOPQRSTUV";
+const chatId = -1234567890;
